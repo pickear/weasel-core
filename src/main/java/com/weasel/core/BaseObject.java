c6d64d09f276a252ee0fdc7e6f558d4f79607d1c
pickear <pickears@gmail.com>
2015-09-14 16:49:45 +0800

    添加注释信息

--- src/main/java/com/weasel/core/BaseObject.java
+++ src/main/java/com/weasel/core/BaseObject.java
@@ -7,13 +7,15 @@ import java.io.Serializable;
 
 import com.weasel.core.annotation.Id;
 
-/**
+/**我们猜想，每个对象都会有一个id作为唯一标识，BaseObject就是有这样的作用。子类继承了BaseObject，就继承了BaseObject的id。
+ * id的类型通过泛型由用户自定义
  * @author Dylan
  * @time 2013-4-17
  */
 public abstract class BaseObject<ID extends Object> implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+
 	
 	@Id
 	protected ID id;
